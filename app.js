c914fb7c692957d951d287011ca8bb6f6d8463a1
sumit <sumitpatil90990@gmail.com>
2021-02-28 00:42:03 +0530

    added response success or failed status

--- app.js
+++ app.js
@@ -41,6 +41,13 @@ app.post("/", function(req, res) {
   }
 
   const request = https.request(url, options, function(response) {
+    if(response.statusCode === 200){
+      res.sendFile(__dirname + "/success.html");
+    }
+    else{
+      res.sendFile(__dirname + "/failure.html");
+    }
+
     response.on('data', (data) => {
       console.log(JSON.parse(data));
     });
@@ -48,6 +55,11 @@ app.post("/", function(req, res) {
   request.write(jsonData);
   request.end();
 
+});
+
+
+app.get("/failure", function(req, res){
+  res.redirect('/');
 });
 
 app.listen(3000, function() {
