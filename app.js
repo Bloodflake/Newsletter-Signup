7d78ca928b96519d31bebf316d89993fef10570f
sumit <sumitpatil90990@gmail.com>
2021-02-27 13:47:58 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.listen(3000, function(){
+  console.log("server is running on 3000");
+});
