4cd831796f942fcbd23fd9b7a316a9fb6ac9633f
sumit <sumitpatil90990@gmail.com>
2021-02-27 18:25:22 +0530

    used api to submit contact list

--- app.js
+++ app.js
@@ -1,18 +1,55 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
+const apiData = require('./config.js');
 
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
-})
+});
 
-app.listen(3000, function(){
+app.post("/", function(req, res) {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.emailName;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us1.api.mailchimp.com/3.0/lists/" + apiData.apiData.listId;
+  const options = {
+    method: "POST",
+    auth: "sumit:" + apiData.apiData.apiKey
+  }
+
+  const request = https.request(url, options, function(response) {
+    response.on('data', (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
+
+});
+
+app.listen(3000, function() {
   console.log("server is running on 3000");
 });
