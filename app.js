19cbd67dbe21ed538f3ff9517b08168504d855ca
sumit <sumitpatil90990@gmail.com>
2021-02-27 14:54:34 +0530

    moved css to public/css folder

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const request = require('request');
 
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.get("/", function(req, res){
